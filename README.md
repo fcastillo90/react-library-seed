f9ec3dd3c629655694f1b67e9870a9bd000cbd03
Francisco Castillo <fcastillog@ripley.com>
2020-07-12 12:21:22 -0400

    FEATURE - init

---
+++ README.md
@@ -0,0 +1,7 @@
+After building/publishing/installing our updated component library, we should be able to import components in two ways:
+
+import { TestComponent } from 'react-component-library';
+
+OR
+
+import TestComponent from 'react-component-library/build/TestComponent';
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+
+const packageJson = require("./package.json");
+
+export default {
+  input: ["src/index.ts", "src/TestComponent/TestComponent.tsx"],
+  output: [
+    {
+      dir: "build",
+      format: "cjs",
+      sourcemap: true
+    }
+  ],
+  preserveModules: true,
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+  ]
+};
